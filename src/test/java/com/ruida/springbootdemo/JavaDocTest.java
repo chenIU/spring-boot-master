2b86607016e1bb7b08bebb05892937dfec46e7a6
windows <1414680900@qq.com>
2020-09-10 22:13:09 +0800

    JavaDoc

---
+++ src/test/java/com/ruida/springbootdemo/JavaDocTest.java
@@ -0,0 +1,20 @@
+package com.ruida.springbootdemo;
+
+/**
+ * @author chenjy
+ * @date 2020-09-10
+ * @since jdk 1.5
+ */
+public class JavaDocTest {
+
+    /**
+     * 计算两个数字的和
+     * @param a 第一个数
+     * @param b 第二个数
+     * @return 两数之和
+     */
+    public int add(int a,int b){
+        return a + b;
+    }
+
+}
