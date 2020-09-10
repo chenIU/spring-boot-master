218762c6b76023af7b68d37d9de7d79a8c99c89a
windows <1414680900@qq.com>
2020-09-11 00:08:47 +0800

    Junit

---
+++ src/test/java/com/ruida/springbootdemo/JunitTest.java
@@ -0,0 +1,27 @@
+package com.ruida.springbootdemo;
+
+import com.ruida.springbootdemo.utils.Calculator;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JunitTest {
+
+    @Before
+    public void init(){
+        System.out.println("init...");
+    }
+
+    @Test
+    public void add(){
+        //System.out.println("this is add method...");
+        int result = new Calculator().add(1,2);
+        Assert.assertEquals(3,result);
+    }
+
+    @After
+    public void destroy(){
+        System.out.println("destroy...");
+    }
+}
---
+++ src/test/java/com/ruida/springbootdemo/utils/Calculator.java
@@ -0,0 +1,8 @@
+package com.ruida.springbootdemo.utils;
+
+public class Calculator {
+
+    public int add (int a,int b){
+        return a + b;
+    }
+}
