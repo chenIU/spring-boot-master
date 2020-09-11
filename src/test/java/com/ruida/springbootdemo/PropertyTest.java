8e7994a8f82e05f32b27e0f50224534ffabddb3c
chenjianyin <chenjianyin@foxmail.com>
2020-09-11 10:06:00 +0800

    properties

---
+++ src/test/java/com/ruida/springbootdemo/PropertyTest.java
@@ -0,0 +1,32 @@
+package com.ruida.springbootdemo;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * @description:
+ * @author: chenjy
+ * @create: 2020-09-11 09:54
+ */
+public class PropertyTest {
+
+    private static Properties properties;
+
+    static{
+        InputStream in = PropertyTest.class.getClassLoader().getResourceAsStream("config/dbconfig.properties");
+        properties = new Properties();
+        try {
+            properties.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        for(Map.Entry<Object,Object> entry:properties.entrySet()){
+            System.out.println(entry.getKey()+"="+entry.getValue());
+        }
+    }
+}
