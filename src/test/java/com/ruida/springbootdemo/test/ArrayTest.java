993ba404496b3c181776ce2c532b9f9b18c9e243
chenjianyin <chenjianyin@foxmail.com>
2020-09-29 10:43:45 +0800

    Arrays.toString & Arrays.deepToString

--- src/test/java/com/ruida/springbootdemo/test/ArrayTest.java
+++ src/test/java/com/ruida/springbootdemo/test/ArrayTest.java
@@ -15,5 +15,13 @@ public class ArrayTest {
         System.arraycopy(arr1,1,arr2,0,3);
         //{2,3,4,9,10}
         System.out.println(Arrays.toString(arr2));
+
+        int[] a = {1,2,3,4};
+        System.out.println(Arrays.toString(a));
+
+        //deepToString打印二维数组;toString打印以为数组,打印二位数组的地址
+        int[][] b = {{1,2,3,4},{5,6,7,8}};
+        System.out.println(Arrays.toString(b));
+        System.out.println(Arrays.deepToString(b));
     }
 }
