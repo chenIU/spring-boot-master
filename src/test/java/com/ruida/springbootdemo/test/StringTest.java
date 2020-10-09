9243d3dd9e511ef567c77475e25966c3630ed171
chenjianyin <chenjianyin@foxmail.com>
2020-10-09 14:57:28 +0800

    two method for string concat

--- src/test/java/com/ruida/springbootdemo/test/StringTest.java
+++ src/test/java/com/ruida/springbootdemo/test/StringTest.java
@@ -1,9 +1,27 @@
 package com.ruida.springbootdemo.test;
 
+import java.util.UUID;
+
 public class StringTest {
     public static void main(String[] args) {
 
-        String str = "Aa";
+        long t1 = System.currentTimeMillis();
+        String str = null;
+        for(int i=0;i<10000;i++){
+            str += UUID.randomUUID();
+        }
+        long t2 = System.currentTimeMillis();
+        System.out.println("字符串拼接耗时："+(t2-t1)+"ms");
+
+        long t3 = System.currentTimeMillis();
+        StringBuffer sb = new StringBuffer();
+        for(int i=0;i<10000;i++){
+            sb.append(UUID.randomUUID());
+        }
+        long t4 = System.currentTimeMillis();
+        System.out.println("StringBuffer append方法耗时："+(t4-t3)+"ms");
+
+        /*String str = "Aa";
         byte[] bytes = str.getBytes();
         for(byte b:bytes){
             System.out.println(b);
@@ -12,7 +30,7 @@ public class StringTest {
         char[] chars = str.toCharArray();
         for(char c:chars){
             System.out.println(c);
-        }
+        }*/
 
        /* String s1 = "123";
         String s2 = "123";
