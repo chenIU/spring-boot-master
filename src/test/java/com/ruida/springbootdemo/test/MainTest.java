a420358addf990a4ed36f655802a2890f3e9d102
windows <1414680900@qq.com>
2020-10-11 12:19:37 +0800

    block thread

---
+++ src/test/java/com/ruida/springbootdemo/test/MainTest.java
@@ -0,0 +1,9 @@
+package com.ruida.springbootdemo.test;
+
+public class MainTest {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(args[0]);
+        System.out.println("111");
+        Thread.sleep(Long.MAX_VALUE);
+    }
+}
