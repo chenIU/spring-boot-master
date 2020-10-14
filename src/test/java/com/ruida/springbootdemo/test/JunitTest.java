a3687b8a7dd3e505133900296634f6c5a7205b6c
chenjianyin <chenjianyin@foxmail.com>
2020-10-14 13:34:03 +0800

    Spring Assert

--- src/test/java/com/ruida/springbootdemo/test/JunitTest.java
+++ src/test/java/com/ruida/springbootdemo/test/JunitTest.java
@@ -1,10 +1,11 @@
 package com.ruida.springbootdemo.test;
 
+import com.ruida.springbootdemo.entity.User;
 import com.ruida.springbootdemo.utils.Calculator;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.util.Assert;
 
 public class JunitTest {
 
@@ -17,11 +18,17 @@ public class JunitTest {
     public void add(){
         //System.out.println("this is add method...");
         int result = new Calculator().add(1,2);
-        Assert.assertEquals(3,result);
+        //Assert.assertEquals(3,result);
     }
 
     @After
     public void destroy(){
         System.out.println("destroy...");
+    }
+
+    @Test
+    public void getUser(){
+        User user = null;
+        Assert.notNull(user,"用户不能为空");
     }
 }
