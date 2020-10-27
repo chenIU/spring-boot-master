7eaef059da2e4913a7e1c7bafa98821943618532
chenjianyin <chenjianyin@foxmail.com>
2020-10-27 11:55:05 +0800

    Knife4j

--- src/main/java/com/ruida/springbootdemo/config/Swagger2Config.java
+++ src/main/java/com/ruida/springbootdemo/config/Swagger2Config.java
@@ -1,7 +1,11 @@
 package com.ruida.springbootdemo.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,29 +15,38 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- * @description:
+ * @description:we
  * @author: chenjy
  * @create: 2020-04-27 16:24
  */
 @Configuration
 @EnableSwagger2
+@EnableKnife4j
+@Import(BeanValidatorPluginsConfiguration.class)
 public class Swagger2Config {
 
-    @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
+    @Value("${server.port}")
+    private String port;
+
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        Docket docket=new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                //分组名称
+                .groupName("2.X版本")
                 .select()
+                //这里指定Controller扫描包路径
                 .apis(RequestHandlerSelectors.basePackage("com.ruida.springbootdemo.controller"))
                 .paths(PathSelectors.any())
                 .build();
+        return docket;
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("swagger-bootstrap-ui RESTful APIs")
                 .description("swagger-bootstrap-ui")
-                .termsOfServiceUrl("http://localhost:8080/")
+                .termsOfServiceUrl("http://localhost:"+port+"/")
                 .contact("developer@mail.com")
                 .version("1.0")
                 .build();
