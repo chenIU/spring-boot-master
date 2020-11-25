7d34ceb1dce717ba9df311bde32f59b5c5269560
chenjianyin <chenjianyin@foxmail.com>
2020-11-25 13:22:11 +0800

    不可变集合

--- src/test/java/com/ruida/springbootdemo/test/MapTest.java
+++ src/test/java/com/ruida/springbootdemo/test/MapTest.java
@@ -1,9 +1,6 @@
 package com.ruida.springbootdemo.test;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * @description: map测试类
@@ -14,14 +11,23 @@ import java.util.TreeMap;
  * @create: 2020-04-28 10:08
  */
 public class MapTest {
+
+//    private static final Map<String,Object> MAP = new HashMap();
+    private static Map<String,Object> MAP = new HashMap();
+
+    {
+        MAP.put("1","jay");
+        MAP.put("2","chou");
+    }
+
     public static void main(String[] args) {
-        /*Map<String,Object> map = new HashMap();
-        map.put("name","chenjy");
-        map.put("age",21);
-        map.put("height",1.72);
-        Iterator<Map.Entry<String, Object>> it = map.entrySet().iterator();
+        /*Map<String,Object> MAP = new HashMap();
+        MAP.put("name","chenjy");
+        MAP.put("age",21);
+        MAP.put("height",1.72);
+        Iterator<Map.Entry<String, Object>> it = MAP.entrySet().iterator();
         while(it.hasNext()){
-            map.remove("name");
+            MAP.remove("name");
             Map.Entry entry = it.next();
             String key = (String) entry.getKey();
             Object value = entry.getValue();
@@ -55,5 +61,13 @@ public class MapTest {
         System.out.println(linkedHashMap);
 
         //HashMap和HashTable都不是有序的
+
+        //确保一个集合不被修改
+        //final修饰一个成员变量，如果是基本数据类型，表示这个变量的值不能被修改。如果是引用类型，则表示这个引用的地址不能改变，但是这个引用指向对象的内容还是可以改变。
+        //unmodifiableMap、unmodifiableList、unmodifiableSet
+        MAP = Collections.unmodifiableMap(MAP);
+        MAP.put("1","boy");
+        System.out.println(MAP.get("1"));
+
     }
 }
