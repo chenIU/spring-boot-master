45145df36b47d04d5df69c18b1dd895e65350e81
chenjianyin <chenjianyin@foxmail.com>
2020-12-08 11:34:08 +0800

    ArrayList 输出

--- src/test/java/com/ruida/springbootdemo/test/ArrayTest.java
+++ src/test/java/com/ruida/springbootdemo/test/ArrayTest.java
@@ -1,6 +1,7 @@
 package com.ruida.springbootdemo.test;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 /**
  * @description:
@@ -10,6 +11,12 @@ import java.util.Arrays;
 public class ArrayTest {
 
     public static void main(String[] args) {
+
+        String[] arr = {"chen","liu","zeng"};
+        System.out.println(arr);
+        Stream.of(arr).forEach(System.out::println);
+        System.out.println(Arrays.toString(arr));
+
         int[] arr1 = {1,2,3,4,5};
         int[] arr2 = {6,7,8,9,10};
         System.arraycopy(arr1,1,arr2,0,3);
@@ -19,7 +26,7 @@ public class ArrayTest {
         int[] a = {1,2,3,4};
         System.out.println(Arrays.toString(a));
 
-        //deepToString打印二维数组;toString打印以为数组,打印二位数组的地址
+        //deepToString打印二维数组;toString打印以为数组,打印二维数组的地址
         int[][] b = {{1,2,3,4},{5,6,7,8}};
         System.out.println(Arrays.toString(b));
         System.out.println(Arrays.deepToString(b));
