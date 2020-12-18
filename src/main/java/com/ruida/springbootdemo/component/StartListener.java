7ebb8ae6e28650dbdd14b1902d2bc4e04db0c187
chenjianyin <chenjianyin@foxmail.com>
2020-12-18 15:12:36 +0800

    property配置类

--- src/main/java/com/ruida/springbootdemo/component/StartListener.java
+++ src/main/java/com/ruida/springbootdemo/component/StartListener.java
@@ -1,9 +1,11 @@
 package com.ruida.springbootdemo.component;
 
+import com.ruida.springbootdemo.config.DBConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+import javax.annotation.Resource;
 
 /**
  * @description: 启动监听
@@ -13,8 +15,15 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class StartListener implements ApplicationListener<ApplicationReadyEvent> {
+
+    @Resource
+    private DBConfig dbConfig;
+
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
         log.error("项目启动监听...");
+        for(Object key:dbConfig.properties.keySet()){
+            log.info("{}={}",key,dbConfig.properties.getProperty((String) key));
+        }
     }
 }
