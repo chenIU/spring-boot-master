d7e450c4d173a31130317bb3c215a709555ac272
chenjianyin <chenjianyin@foxmail.com>
2020-12-24 11:24:50 +0800

    集合的有序性

--- src/test/java/com/ruida/springbootdemo/utils/CollectionTest.java
+++ src/test/java/com/ruida/springbootdemo/utils/CollectionTest.java
@@ -1,7 +1,6 @@
 package com.ruida.springbootdemo.utils;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @description: 集合测试类
@@ -12,7 +11,28 @@ public class CollectionTest {
 
     public static void main(String[] args) {
 
+        //集合有序性测试
 
+        //HashMap是无须的
+        Map<String,Object> map = new HashMap();
+        map.put("name","chenjy");
+        map.put("age",25);
+        map.put("school","lit");
+        System.out.println(map);
+
+        //LinkedHashMap是有序的
+        LinkedHashMap linkedHashMap = new LinkedHashMap();
+        linkedHashMap.put("name","chenjy");
+        linkedHashMap.put("age",25);
+        linkedHashMap.put("school","lit");
+        System.out.println(linkedHashMap);
+
+        //ArrayList是有序的
+        List<String> numberList = new ArrayList<>();
+        numberList.add("1");
+        numberList.add("2");
+        numberList.add("3");
+        System.out.println(numberList);
 
         int[] arr1 = new int[]{1,2,3};
         int[] arr2 = arr1;
