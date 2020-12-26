21767f75a019a81afd13f270b1d55e75b212e828
mi <1414680900@qq.com>
2020-12-26 22:43:53 +0800

    采用匿名内部类的方式指定比较器

--- src/test/java/com/ruida/springbootdemo/test/CollectionsTest.java
+++ src/test/java/com/ruida/springbootdemo/test/CollectionsTest.java
@@ -1,8 +1,10 @@
 package com.ruida.springbootdemo.test;
 
 import com.google.common.collect.Lists;
+import com.ruida.springbootdemo.entity.Room;
 import org.junit.Test;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -42,6 +44,28 @@ public class CollectionsTest {
         List<String> list = Lists.newArrayList("tom","mike","jack");
         Collections.sort(list);
         System.out.println(list);
+
+        List<Room> roomList = Lists.newArrayList(
+                new Room(String.valueOf(5.00), 4.00, 2.00),
+                new Room(String.valueOf(5.00), 4.01, 3.01),
+                new Room(String.valueOf(6.00), 4.01, 5.00),
+                new Room(String.valueOf(6.00), 4.01, 4.50),
+                new Room(String.valueOf(6.00), 4.01, 6.00),
+                new Room(String.valueOf(7.00), 4.02, 3.05)
+        );
+        System.out.println("自定义类排序前:"+roomList);
+        //使用实体类自带的排序规则(升序)
+//        Collections.sort(roomList);
+
+        //自定义排序规则(降序)
+        //自定义的排序规则优先级高
+        Collections.sort(roomList, new Comparator<Room>() {
+            @Override
+            public int compare(Room o1, Room o2) {
+                return o2.getHeight().compareTo(o1.getHeight());
+            }
+        });
+        System.out.println("自定义类排序前:"+roomList);
     }
 
     /**
