0537efb492efc936ceacd52e1e8c8e96c7ed5bcd
chenjianyin <chenjianyin@foxmail.com>
2020-12-28 17:13:58 +0800

    集合中removeAll和clear方法的区别

--- src/test/java/com/ruida/springbootdemo/test/ArrayListTest.java
+++ src/test/java/com/ruida/springbootdemo/test/ArrayListTest.java
@@ -1,6 +1,7 @@
 package com.ruida.springbootdemo.test;
 
 import com.google.common.collect.Lists;
+import org.junit.Test;
 
 import java.util.ArrayList;
 
@@ -22,5 +23,20 @@ public class ArrayListTest {
         System.out.println(list);
 
         //原因：System.arraycopy,在进行数据移除的时候,是对原数组进行拷贝
+    }
+
+    @Test
+    public void test(){
+        ArrayList<Integer> list1 = Lists.newArrayList(1,2,3);
+        ArrayList<Integer> list2 = Lists.newArrayList(0,1,2,3,4,5);
+        list2.removeAll(list1);
+        System.out.println(list2);
+
+        //removeAll可以理解为批量移除,先求两个集合的交集,移除之,保留剩下的部分
+
+        list2.clear();
+        System.out.println(list2);
+        System.out.println(list2 == null);
+        //clear即是字面意思,将集合中所有元素清空
     }
 }
