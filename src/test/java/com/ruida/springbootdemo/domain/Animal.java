68e0df78dd7be977513d63d6171ab9cb9109f4a2
chenjianyin <chenjianyin@foxmail.com>
2020-12-31 17:23:06 +0800

    this && super

---
+++ src/test/java/com/ruida/springbootdemo/domain/Animal.java
@@ -0,0 +1,13 @@
+package com.ruida.springbootdemo.domain;
+
+/**
+ * @description:
+ * @author: chenjy
+ * @create: 2020-12-31 17:09
+ */
+public abstract class Animal {
+
+    public void eat(){
+        System.out.println("Animal eat...");
+    }
+}
--- src/test/java/com/ruida/springbootdemo/domain/Dog.java
+++ src/test/java/com/ruida/springbootdemo/domain/Dog.java
@@ -6,4 +6,9 @@ package com.ruida.springbootdemo.domain;
  * @create: 2020-12-28 10:34
  */
 public class Dog extends Mammal{
+
+    public void eatAndSleep(){
+        super.eat();//super继承最近的父类或者接口
+        System.out.println("Dog sleep...");
+    }
 }
