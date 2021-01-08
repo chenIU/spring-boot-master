bb6bee8952a81a715082b052295e1f3fa81e1ada
chenjianyin <1414680900@qq.com>
2021-01-08 09:54:28 +0800

    @EqualsAndHashCode

--- src/main/java/com/ruida/springbootdemo/entity/Blog.java
+++ src/main/java/com/ruida/springbootdemo/entity/Blog.java
@@ -1,17 +1,20 @@
 package com.ruida.springbootdemo.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 /**
  * @description: 博客
+ * 使用@Data模式@EqualsAndHashCode(callsuper=false)
+ * callsuper=false表示对象在比较时不会考虑父类对象中的属性,仅仅比较子类中的属性
+ * callsuper=true在比较时会考虑父类中的属性,通过父类和子类中的属性一起判断是否相等
  * @author: chenjy
  * @create: 2020-06-12 09:03
  */
 @Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class Blog implements Cloneable{
 
     private int id;
