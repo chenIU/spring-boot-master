b65e2604172b862de0927ff4740d90eb1f5948ca
chenjianyin <1414680900@qq.com>
2021-01-08 17:37:47 +0800

    @ConditionalOnProperty在配置类中的使用

--- src/main/java/com/ruida/springbootdemo/config/JacksonConfig.java
+++ src/main/java/com/ruida/springbootdemo/config/JacksonConfig.java
@@ -3,7 +3,9 @@ package com.ruida.springbootdemo.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ruida.springbootdemo.serializer.modifier.CustomizeBeanSerializerModifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
@@ -12,7 +14,12 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
  * @author: chenjy
  * @create: 2021-01-08 15:46
  */
-//@Configuration
+@Configuration
+@ConditionalOnProperty(prefix = "customize.null-value",name = "enable",havingValue = "true")
+//prefix：配置文件中的前缀
+//name：配置的名字
+//havingValue：与配置值的对比值,当两个值相同返回true,配置类生效
+//matchIfMissing：缺省时是否生效
 public class JacksonConfig {
 
     @Bean
