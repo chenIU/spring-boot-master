ec74e0e8a36bb01c0702f7ab8bc7b1ad085082b9
chenjianyin <1414680900@qq.com>
2021-01-29 08:51:09 +0800

    对象排序升降序问题

--- src/main/java/com/ruida/springbootdemo/entity/City.java
+++ src/main/java/com/ruida/springbootdemo/entity/City.java
@@ -1,6 +1,8 @@
 package com.ruida.springbootdemo.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @description:
@@ -8,9 +10,18 @@ import lombok.Data;
  * @create: 2020-10-30 11:15
  */
 @Data
-public class City {
+@AllArgsConstructor
+@NoArgsConstructor
+public class City implements Comparable<City>{
 
     private String cityName;
 
     private Integer cityCode;
+
+    @Override
+    public int compareTo(City o) {
+        //o1比o2升序排序,o2比o1降序排序
+//        return this.cityCode.compareTo(o.getCityCode());
+        return o.getCityCode().compareTo(this.cityCode);
+    }
 }
