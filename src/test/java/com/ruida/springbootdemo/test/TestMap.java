cda6eae7d4fac0877e67b3474982b093ccdbe78e
chenjianyin <1414680900@qq.com>
2021-03-08 16:33:44 +0800

    遍历Map

--- src/test/java/com/ruida/springbootdemo/test/TestMap.java
+++ src/test/java/com/ruida/springbootdemo/test/TestMap.java
@@ -5,6 +5,15 @@ import java.util.Collections;
 import java.util.List;
 
 /**
+ * 遍历Map的七种方式：
+ * 1.迭代器EntrySet
+ * 2.迭代器KeySet
+ * 3.ForEach EntrySet
+ * 4.ForEach KeySet
+ * 5.lambda
+ * 6.Stream
+ * 7.parallelStream
+ * 一般建议使用迭代EntrySet的方式，可以避免并发修改异常，更加直观
  * @description:
  * @author: chenjy
  * @create: 2020-05-18 15:05
@@ -20,6 +29,5 @@ public class TestMap {
         list.sort(Integer::compareTo);
 
         list.forEach(System.out::println);
-
     }
 }
