11926211b32035a2a6e37511020dc2c6ac070623
chenjianyin <1414680900@qq.com>
2021-03-08 13:45:08 +0800

    Double multiply NPE

--- src/test/java/com/ruida/springbootdemo/test/DoubleTest.java
+++ src/test/java/com/ruida/springbootdemo/test/DoubleTest.java
@@ -1,5 +1,7 @@
 package com.ruida.springbootdemo.test;
 
+import org.junit.Test;
+
 /**
  * @description:
  * @author: chenjy
@@ -9,5 +11,17 @@ public class DoubleTest {
     public static void main(String[] args) {
         Double d = new Double(3.4);
         System.out.println(d.intValue());
+    }
+
+    /**
+     * double类型的数字相乘(不建议),乘数不能为null
+     */
+    @Test
+    public void test(){
+        Double a = 0.1;
+        Double b = 0.2;
+        Double c = null;
+        System.out.println(a*b);
+        System.out.println(a*c);
     }
 }
