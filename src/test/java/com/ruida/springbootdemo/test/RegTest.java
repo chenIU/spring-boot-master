615264396d631c55502e4959f0391893b3ad7e41
perseus <1414680900@qq.com>
2021-07-17 17:34:09 +0800

    正则表达式判断手机号和中文

--- src/test/java/com/ruida/springbootdemo/test/RegTest.java
+++ src/test/java/com/ruida/springbootdemo/test/RegTest.java
@@ -1,5 +1,9 @@
 package com.ruida.springbootdemo.test;
 
+import org.junit.Test;
+
+import java.util.regex.Pattern;
+
 /**
  * @author Chen.J.Y
  * @date 2021/5/22
@@ -14,5 +18,23 @@ public class RegTest {
     public static boolean checkImageExtension(String suffix){
         //(?i)表示不区分大小写
         return suffix.matches("^(?i)(jpg|jpeg|png|bmp|gif|psd)");
+    }
+
+    /**
+     * 校验手机号
+     */
+    @Test
+    public void testPhone(){
+        String phone = "18848956818";
+        System.out.println(Pattern.matches("^[1][3,4,5,6,7,8,9][0-9]{9}", phone));
+    }
+
+    /**
+     * 校验中文字符
+     */
+    @Test
+    public void testChinese(){
+        String str = "中国";
+        System.out.println(Pattern.matches("^[\\u4e00-\\u9fa5]*$", str));
     }
 }
