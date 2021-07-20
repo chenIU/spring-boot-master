899b151d48a14e84b6d45a3ddf85c89ac92d4baf
perseus <1414680900@qq.com>
2021-07-20 09:04:55 +0800

    @Value

--- src/test/java/com/ruida/springbootdemo/test/boot/ApplicationTest.java
+++ src/test/java/com/ruida/springbootdemo/test/boot/ApplicationTest.java
@@ -6,11 +6,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,6 +23,18 @@ import java.util.Map;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringBootDemoApplication.class)
 public class ApplicationTest implements ApplicationContextAware {
+
+    /**
+     * : 的作用是给一个默认值，在yml文件中没有该配置时，也不会报错
+     */
+    @Value("${test.name:}")
+    private String name;
+
+    /**
+     * 利用EL表达式解析yml文件中的list数据结构，set、map与此类似
+     */
+    @Value("#{'${test.list:}'.empty ? null : '${test.list:}'.split(',')}")
+    private List<String> stringList;
 
     private ApplicationContext applicationContext;
 
@@ -36,5 +50,15 @@ public class ApplicationTest implements ApplicationContextAware {
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
+    }
+
+    @Test
+    public void test2(){
+        System.out.println(name);
+    }
+
+    @Test
+    public void test3(){
+        stringList.forEach(System.out::println);
     }
 }
