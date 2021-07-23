332b3a25f9fd27cbac7aad5e25696faeadeb43d0
perseus <1414680900@qq.com>
2021-07-23 09:05:40 +0800

    数组小技巧

--- src/test/java/com/ruida/springbootdemo/test/ArrayListTest.java
+++ src/test/java/com/ruida/springbootdemo/test/ArrayListTest.java
@@ -78,4 +78,31 @@ public class ArrayListTest {
                 iterator.remove();
         }
     }
+
+    /**
+     * int getDays(int month){
+     *         if (month == 1)  return 31;
+     *         if (month == 2)  return 29;
+     *         if (month == 3)  return 31;
+     *         if (month == 4)  return 30;
+     *         if (month == 5)  return 31;
+     *         if (month == 6)  return 30;
+     *         if (month == 7)  return 31;
+     *         if (month == 8)  return 31;
+     *         if (month == 9)  return 30;
+     *         if (month == 10)  return 31;
+     *         if (month == 11)  return 30;
+     *         if (month == 12)  return 31;
+     *     }
+     */
+    int[] monthDays = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    int getDays(int month){
+        return monthDays[--month];
+    }
+
+    @Test
+    public void test6(){
+        System.out.println(getDays(2));
+    }
 }
